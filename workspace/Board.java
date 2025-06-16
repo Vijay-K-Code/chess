ea9414a46b4f7caf02332554710249295df542c7
Vijay-K-Code <vijaysantoshkannan@gmail.com>
2025-06-16 23:24:52 +0000

    update

--- workspace/Board.java
+++ workspace/Board.java
@@ -15,6 +15,16 @@ import javax.swing.*;
 public class Board extends JPanel implements MouseListener, MouseMotionListener {
     private static final String RESOURCES_WROOK_PNG = "wrook.png";
     private static final String RESOURCES_BROOK_PNG = "brook.png";
+    private static final String RESOURCES_BKNIGHT_PNG = "bknight.png";
+    private static final String RESOURCES_WKNIGHT_PNG = "wknight.png";
+    private static final String RESOURCES_BBISHOP_PNG = "bbishop.png";
+    private static final String RESOURCES_WBISHOP_PNG = "wbishop.png";
+    private static final String RESOURCES_BPAWN_PNG = "bpawn.png";
+    private static final String RESOURCES_WPAWN_PNG = "wpawn.png";
+    private static final String RESOURCES_BQUEEN_PNG = "bqueen.png";
+    private static final String RESOURCES_WQUEEN_PNG = "wqueen.png";
+    private static final String RESOURCES_BKING_PNG = "bking.png";
+    private static final String RESOURCES_WKING_PNG = "wking.png";
 
     private final Square[][] board;
     private final GameWindow g;
@@ -57,8 +67,20 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
     // Sets up rooks for each player
     private void initializePieces() {
         board[0][0].put(new Rook(false, RESOURCES_BROOK_PNG)); // Black Rook
+        board[0][1].put(new Knight(false, RESOURCES_BKNIGHT_PNG));//Black Knight
+        board[0][2].put(new Bishop(false, RESOURCES_BBISHOP_PNG)); // Black Bishop
+        board[0][3].put(new Queen(false, RESOURCES_BQUEEN_PNG)); // Black Queen
+        board[0][4].put(new King(false, RESOURCES_BKING_PNG)); // Black King
+        board[0][5].put(new Bishop(false, RESOURCES_BBISHOP_PNG)); // Black Bishop
+        board[0][6].put(new Knight(false, RESOURCES_BKNIGHT_PNG)); // Black Knight
         board[0][7].put(new Rook(false, RESOURCES_BROOK_PNG)); // Black Rook
         board[7][0].put(new Rook(true, RESOURCES_WROOK_PNG));  // White Rook
+        board[7][1].put(new Knight(true, RESOURCES_WKNIGHT_PNG)); // White Knight
+        board[7][2].put(new Bishop(true, RESOURCES_WBISHOP_PNG)); // White Bishop
+        board[7][3].put(new Queen(true, RESOURCES_WQUEEN_PNG)); // White Queen
+        board[7][4].put(new King(true, RESOURCES_WKING_PNG)); // White King
+        board[7][5].put(new Bishop(true, RESOURCES_WBISHOP_PNG)); // White Bishop
+        board[7][6].put(new Knight(true, RESOURCES_WKNIGHT_PNG)); // White Knight
         board[7][7].put(new Rook(true, RESOURCES_WROOK_PNG));  // White Rook
     }
 
--- workspace/Rook.java
+++ workspace/Rook.java
@@ -1,60 +1,150 @@
+//Spencer Gilcrest
+//This Piece is a Rook
+//The Piece can move straight in all 4 directions for any distance, but cannot move past other peices
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.imageio.ImageIO;
 
-public class Rook extends Piece {
-
-    public Rook(boolean color, String img_file) {
-        super(color, img_file);
+//you will need to implement two functions in this file.
+public class Rook extends Piece{
+    
+    public Rook(boolean isWhite, String img_file) {
+        super(isWhite, img_file);
     }
 
-    // Controlled squares = legal moves for a Rook
-    public ArrayList<Square> getControlledSquares(Board board, Square start) {
-        return getLegalMoves(board, start);
+    public String toString(){
+      boolean checkColor = this.getColor();
+      String result;
+      if (checkColor){
+        result = "A White Rook";
+      }
+      else {
+        result = "A Black Rook";
+      }
+      return result;
+        
+    }
+        
+    // TO BE IMPLEMENTED!
+    //return a list of every square that is "controlled" by this piece. A square is controlled
+    //if the piece capture into it legally.
+
+    //pre condition: board is 2d array of squares. start is a square and been initialized.
+    //post condition: returns arrayList of squares that the piece could theorhetically capture into on the next move
+    public ArrayList<Square> getControlledSquares(Square[][] board, Square start) {
+     ArrayList<Square> controlledSquares = new ArrayList<>();
+    int boardSize = board.length;
+    int startRow = start.getRow();
+    int startCol = start.getCol();
+
+    for (int row = startRow - 1; row>=0;  row--){
+      controlledSquares.add(board[row][startCol]);
+      if (board[row][startCol].getOccupyingPiece() != null){
+        break;
+      }
     }
 
-    @Override
-    public ArrayList<Square> getLegalMoves(Board board, Square start) {
-        ArrayList<Square> moves = new ArrayList<>();
-        Square[][] squares = board.getSquareArray();
+    for (int row = startRow + 1; row<boardSize;  row++){
+      controlledSquares.add(board[row][startCol]);
+      if (board[row][startCol].getOccupyingPiece() != null){
+        break;
+      }
+    }
 
-        int row = start.getRow();
-        int col = start.getCol();
+    for (int col = startCol - 1; col>=0; col--){
+      controlledSquares.add(board[startRow][col]);
+      if (board[startRow][col].getOccupyingPiece() != null){
+        break;
+      }
+    }
 
-        int[][] directions = {
-            {1, 0},  // Down
-            {-1, 0}, // Up
-            {0, 1},  // Right
-            {0, -1}  // Left
-        };
+    for (int col = startCol + 1; col < boardSize; col++){
+      controlledSquares.add(board[startRow][col]);
+      if (board[startRow][col].getOccupyingPiece() != null){
+        break;
+      }
+    }
+    return controlledSquares;
+    }
+    
 
-        for (int[] dir : directions) {
-            for (int i = 1; i <= 3; i++) {
-                int newRow = row + i * dir[0];
-                int newCol = col + i * dir[1];
+    //TO BE IMPLEMENTED!
+    //implement the move function here
+    //it's up to you how the piece moves, but at the very least the rules should be logical and it should never move off the board!
+    //returns an arraylist of squares which are legal to move to
+    //please note that your piece must have some sort of logic. Just being able to move to every square on the board is not
+    //going to score any points.
 
-                if (!isValidPosition(newRow, newCol)) break;
+    //pre condition: b is of type board and has been properly initialized. start is of type square and has been properly initialized.
+    //post condition: returns arrayList of possible moves such that the piece moves like a rook. moves straight in all 4 directions and cannot pass over other pieces. 
+    public ArrayList<Square> getLegalMoves(Board b, Square start){
+      ArrayList<Square> legalMoves = new ArrayList<>();
+      int currentRow = start.getRow();
+      int currentCol = start.getCol();
 
-                Square target = squares[newRow][newCol];
+      Square[][] board  = b.getSquareArray();
+      int boardSize = board.length;
 
-                if (target.isOccupied()) {
-                    if (target.getOccupyingPiece().getColor() != this.getColor()) {
-                        moves.add(target); // Capture
-                    }
-                    break; // Blocked
-                }
+      boolean isWhite = this.getColor();
 
-                moves.add(target);
-            }
+      for (int col = currentCol + 1; col < boardSize; col++){
+        Square tempSquare = board[currentRow][col];
+        if (tempSquare.getOccupyingPiece() == null){
+          legalMoves.add(tempSquare);
         }
+        else {
+          if(tempSquare.isOccupied() && tempSquare.getOccupyingPiece().getColor() != isWhite){
+            legalMoves.add(tempSquare);
+          }
+          break;
+        }
+      }
 
-        return moves;
-    }
+      for (int col = currentCol - 1; col >=0; col--){
+        Square tempSquare = board[currentRow][col];
+        if (tempSquare.getOccupyingPiece() == null){
+          legalMoves.add(tempSquare);
+        }
+        else {
+          if(tempSquare.isOccupied() && tempSquare.getOccupyingPiece().getColor() != isWhite){
+            legalMoves.add(tempSquare);
+          }
+          break;
+        }
+      }
 
-    private boolean isValidPosition(int row, int col) {
-        return row >= 0 && row < 8 && col >= 0 && col < 8;
+      for (int row = currentRow - 1; row >= 0; row--){
+        Square tempSquare = board[row][currentCol];
+        if (tempSquare.getOccupyingPiece() == null){
+          legalMoves.add(tempSquare);
+        }
+        else{
+          if (tempSquare.isOccupied() && tempSquare.getOccupyingPiece().getColor() != isWhite){
+            legalMoves.add(tempSquare);
+          }
+          break;
+        }
+      }
+
+      for (int row = currentRow + 1; row < boardSize; row++){
+        Square tempSquare = board[row][currentCol];
+        if (tempSquare.getOccupyingPiece() == null){
+          legalMoves.add(tempSquare);
+        }
+        else{
+          if (tempSquare.isOccupied() && tempSquare.getOccupyingPiece().getColor() != isWhite){
+            legalMoves.add(tempSquare);
+          }
+          break;
+        }
+      }
+      return legalMoves;
     }
 }
